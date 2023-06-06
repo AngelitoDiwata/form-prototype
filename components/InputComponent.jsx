0cdb4c13dcccb0deaf21a317c8dd0cfdcb5f82b7
AngelitoDiwata <diwataangelito@gmail.com>
2023-06-07 00:25:11 +0800

    Added an object parser and formatter page

--- components/InputComponent.jsx
+++ components/InputComponent.jsx
@@ -1,28 +1,11 @@
 import React, { useEffect } from 'react'
 
 export default function InputComponent({ type, title, onChange }) {
-  const typeRef = {
-    varchar: 'text',
-    date: 'date',
-    timestamp: 'date',
-    double: 'number',
-    tinyint: 'number'
-  }
-
-  const returnType = () => {
-    try {
-      return typeRef[Object.keys(typeRef).filter((item) => {
-        return type.Type.includes(item)
-      })[0]]
-    } catch (_) {
-      return null
-    }
-  }
 
   return (
     <div className='flex grow flex-col items-start justify-start space-y-1 my-2 mx-1'>
       <span className='text-gray-500'>{title.replaceAll('_', ' ')}</span>
-      <input type={returnType()} placeholder={title} onChange={(e) => { onChange(e) }} className="input input-bordered input-primary w-full max-w-xs" />
+      <input type={type} placeholder={title} onChange={(e) => { onChange(e) }} className="input input-bordered input-primary w-full max-w-xs" />
     </div>
   )
 }
---
+++ pages/formmatter.jsx
@@ -0,0 +1,74 @@
+import React, { Component, useEffect, useState } from 'react'
+import InputComponent from '../components/InputComponent'
+
+export default function formmatter() {
+    const [formData, setFormData] = useState({
+        title: "Create new product",
+        fields: [
+            {
+                label: "username",
+                type: "text",
+                validations: {
+                    notEmpty: true
+                }
+            },
+            {
+                label: "password",
+                type: "password",
+                validations: {
+                    notEmpty: true
+                }
+            },
+            {
+                label: "birthday",
+                type: "date",
+                validations: {
+                    notEmpty: true
+                }
+            }
+        ],
+        actionQuery: ""
+    })
+
+    const [dataModel, setDataModel] = useState({})
+
+    const setModel = (item, value = '') => {
+        setDataModel((prevState) => ({ ...prevState, [item]: value }))
+    }
+
+    const inputComponents = {
+        text: InputComponent,
+        password: InputComponent,
+        date: InputComponent,
+    }
+
+    useEffect(() => {
+        console.log(dataModel)
+    }, [dataModel])
+
+    const renderFormData = (data) => {
+        return (
+            <React.Fragment>
+                {
+                    data.fields.map((field, index) => {
+                        const Component = inputComponents[field.type]
+                        return <Component type={field.type} title={field.label} onChange={(e) => setModel(field.label, e.target.value)} />
+                    })
+                }
+            </React.Fragment>
+        )
+    }
+
+
+    return (
+        <div className='w-fit max-w-2xl py-5 m-auto'>
+            <h1 className='font-bold text-xl'>{formData.title}</h1>
+            <form className="w-full h-fit flex flex-wrap items-center m-auto">
+                {
+                    renderFormData(formData)
+                }
+            </form>
+        </div>
+
+    )
+}
